5f170329db51159367d73f2dd0f8242e0da172f9
Luciano Peluzi <ladpeluzi@hotmail.com>
2024-01-07 00:04:39 -0300

    Ajustes e início do desafio

--- 01. alura - java - java aplicando a orientação a objetos/screenmatch/src/br/com/alura/desafio/Carro.java
+++ 01. alura - java - java aplicando a orientação a objetos/screenmatch/src/br/com/alura/desafio/Carro.java
@@ -1,38 +1,58 @@
 package br.com.alura.desafio;
 
 public class Carro {
-    int ano;
-    int preco;
-    String modelo;
+    private String modelo;
+    private double precoAno1;
+    private double precoAno2;
+    private double precoAno3;
 
-
-    public int getAno() {
-        return ano;
-    }
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-    public int getPreco() {
-        return preco;
-    }
-    public void setPreco(int preco) {
-        this.preco = preco;
-    }
-    public void getModelo(int ano) {
-
-        if (ano > 2024) {
-            modelo = "Hatch";
-        } else if (ano == 2023) {
-            modelo = "SUV";
-        } else {
-            modelo = "Picapes";
-        }
-        System.out.println("No ano " + ano + " o modelo é " + modelo );
-
-    }
-    public void setModelo(String modelo) {
+    public void definirModelo(String modelo) {
         this.modelo = modelo;
     }
+
+    public void definirPrecos(double precoAno1, double precoAno2, double precoAno3) {
+        this.precoAno1 = precoAno1;
+        this.precoAno2 = precoAno2;
+        this.precoAno3 = precoAno3;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Preço Ano 1: " + precoAno1);
+        System.out.println("Preço Ano 2: " + precoAno2);
+        System.out.println("Preço Ano 3: " + precoAno3);
+        System.out.println("Menor Preço: " + calcularMenorPreco());
+        System.out.println("Maior Preço: " + calcularMaiorPreco());
+    }
+
+    private double calcularMenorPreco() {
+        double menorPreco = precoAno1;
+
+        if (precoAno2 < menorPreco) {
+            menorPreco = precoAno2;
+        }
+
+        if (precoAno3 < menorPreco) {
+            menorPreco = precoAno3;
+        }
+
+        return menorPreco;
+    }
+
+    private double calcularMaiorPreco() {
+        double maiorPreco = precoAno1;
+
+        if (precoAno2 > maiorPreco) {
+            maiorPreco = precoAno2;
+        }
+
+        if (precoAno3 > maiorPreco) {
+            maiorPreco = precoAno3;
+        }
+
+        return maiorPreco;
+    }
+
 
 
 
